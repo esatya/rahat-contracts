9090d37543e82e98866ddc7fde3c77750c926d4e
Manjik Shrestha <myanz.470shrestha@gmail.com>
2021-11-23 10:57:25 +0545

    deploy script

--- truffle-config.js
+++ truffle-config.js
@@ -169,7 +169,7 @@ module.exports = {
         // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
           enabled: true,
-          runs: 200,
+          runs: 100,
         },
       },
     },

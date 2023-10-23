2994f3fbfc9d1002688f2ba05cd50d6db617735f
Poudel0 <poudel0sandesh@gmail.com>
2023-10-23 11:46:39 +0545

    Fixed functionalities for FspSafe.sol

--- contracts/FspSafe.sol
+++ contracts/FspSafe.sol
@@ -8,21 +8,50 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title FSP wallet contract
 /// @author Rumsan Associates
-/// @notice This contract is for banks to receive Rahat tokens and transfer when needed 
+/// @notice This contract is for banks to receive Rahat tokens and transfer when needed
 /// @dev All function calls are only executed by contract owner
 
-
 contract FspSafe is Ownable {
-    constructor(){}
-    transferERC20(address tokenAddress, address to, uint256 amount) OnlyOwner {
-        IERC20(tokenAddress).transfer(to, amount);
+    constructor() {}
+
+    function transferERC20(
+        address tokenAddress,
+        address to,
+        uint256 amount
+    ) public onlyOwner {
+        bool success = IERC20(tokenAddress).transfer(to, amount);
+        require(success, "Transfer Failed");
     }
 
-    transferERC1155(address tokenAddress, address to, uint256 id, uint256 value)  OnlyOwner  {
-        IERC1155(tokenAddress).safeTransferFrom(address(this), to, id, value);
+    function transferERC1155(
+        address tokenAddress,
+        address to,
+        uint256 id,
+        uint256 value,
+        bytes calldata data
+    ) public onlyOwner {
+        IERC1155(tokenAddress).safeTransferFrom(
+            address(this),
+            to,
+            id,
+            value,
+            data
+        );
     }
 
-    transferERC1155Batch(address tokenAddress, address to, uint256[] ids, uint256[] values)  OnlyOwner {
-        IERC1155(tokenAddress).safeTransferFrom(address(this), to, ids, values);
+    function transferERC1155Batch(
+        address tokenAddress,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory values,
+        bytes calldata data
+    ) public onlyOwner {
+        IERC1155(tokenAddress).safeBatchTransferFrom(
+            address(this),
+            to,
+            ids,
+            values,
+            data
+        );
     }
 }

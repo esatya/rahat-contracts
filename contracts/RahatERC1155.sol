81268422cfc7de3a48ace9e7e1ea50b9401b8e41
Poudel0 <poudel0sandesh@gmail.com>
2023-10-24 17:03:26 +0545

    Replaced use of tx.origin with msg.sender

--- contracts/RahatERC1155.sol
+++ contracts/RahatERC1155.sol
@@ -2,107 +2,106 @@
 pragma solidity 0.8.7;
 
 //ERC1155 Tokens
-import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
-import '@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol';
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 
 //Utils
 import "@openzeppelin/contracts/utils/Counters.sol";
-import '@openzeppelin/contracts/access/AccessControl.sol';
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract RahatERC1155 is ERC1155,ERC1155Supply,ERC1155Burnable{
-    
+contract RahatERC1155 is ERC1155, ERC1155Supply, ERC1155Burnable {
     using Counters for Counters.Counter;
     using Strings for uint256;
-    
+
     Counters.Counter private _itemIds; // tracks the token ids
-    
-	mapping(address => bool) public owner;
-	mapping(uint256 => string) public name;
-	mapping(uint256 => string) public symbol;
 
-	modifier OnlyOwner {
-		require(owner[tx.origin], 'Only Admin can execute this transaction');
-		_;
-	}
+    mapping(address => bool) public owner;
+    mapping(uint256 => string) public name;
+    mapping(uint256 => string) public symbol;
 
-	constructor(address _admin) ERC1155("https://ipfs.rumsan.com/ipfs") {
-		owner[_admin] = true;
-	}
-	
+    modifier OnlyOwner() {
+        require(owner[msg.sender], "Only Admin can execute this transaction");
+        _;
+    }
 
-	///@dev Mint New NFT to the caller
+    constructor(address _admin) ERC1155("https://ipfs.rumsan.com/ipfs") {
+        owner[_admin] = true;
+    }
+
+    ///@dev Mint New NFT to the caller
     ///@param _name name of NFT
     ///@param _symbol symbol of NFT
     ///@param _amount amount of NFT
-	function mintERC1155(	string memory _name,
-		string memory _symbol,uint256 _amount) public returns (uint256) {
-	   _itemIds.increment();
-	   uint256 newItemId = _itemIds.current();
-	   name[newItemId] = _name;
-	   symbol[newItemId] = _symbol;
-	    _mint(msg.sender, newItemId, _amount, "");
-	    
-	    return newItemId;
-	}
-	
-    ///@dev set the baseURI of NFT
-    ///@param newuri base URI(eg: https://ipfs.io/ipfs)
-	function setBaseURI(string memory newuri) public OnlyOwner {
-        _setURI(newuri);
-    }
-    
-    ///@dev check if the given tokenId exists
-    ///@param _id ERC1155 tokenId
-    function exists(uint256 _id) public view override returns(bool){
-         return(_itemIds.current() >= _id);
+    function mintERC1155(
+        string memory _name,
+        string memory _symbol,
+        uint256 _amount
+    ) public returns (uint256) {
+        _itemIds.increment();
+        uint256 newItemId = _itemIds.current();
+        name[newItemId] = _name;
+        symbol[newItemId] = _symbol;
+        _mint(msg.sender, newItemId, _amount, "");
+
+        return newItemId;
     }
 
-	///@dev mint ERC1155 token of given tokenId
+    ///@dev set the baseURI of NFT
+    ///@param newuri base URI(eg: https://ipfs.io/ipfs)
+    function setBaseURI(string memory newuri) public OnlyOwner {
+        _setURI(newuri);
+    }
+
+    ///@dev check if the given tokenId exists
+    ///@param _id ERC1155 tokenId
+    function exists(uint256 _id) public view override returns (bool) {
+        return (_itemIds.current() >= _id);
+    }
+
+    ///@dev mint ERC1155 token of given tokenId
     ///@param _id ERC1155 tokenid
-    ///@param _amount amount of ERC1155 token to be minted 
-	function mintExistingERC1155(uint256 _id,uint256 _amount) public {
-	    require(exists(_id),"token with given id doesn't exists");
-	    _mint(msg.sender, _id, _amount, "");
-	}
-	
+    ///@param _amount amount of ERC1155 token to be minted
+    function mintExistingERC1155(uint256 _id, uint256 _amount) public {
+        require(exists(_id), "token with given id doesn't exists");
+        _mint(msg.sender, _id, _amount, "");
+    }
+
     ///@dev Get the token URI of given tokenId
     ///@param _tokenId ERC1155 tokenId
-	function getTokenData(uint256 _tokenId) public view returns(string memory){
-	    return _toFullURI(uri(_tokenId),_tokenId);
-	}
-	
-    function _toFullURI(string memory _baseUri, uint256 _tokenId)
-        internal
-        pure
-        returns (string memory)
-    {
+    function getTokenData(
+        uint256 _tokenId
+    ) public view returns (string memory) {
+        return _toFullURI(uri(_tokenId), _tokenId);
+    }
+
+    function _toFullURI(
+        string memory _baseUri,
+        uint256 _tokenId
+    ) internal pure returns (string memory) {
         return
             string(
-                abi.encodePacked(
-                    _baseUri,
-                    "/",
-                    _tokenId.toString(),
-                    ".json"
-                )
+                abi.encodePacked(_baseUri, "/", _tokenId.toString(), ".json")
             );
     }
 
     function addOwner(address _account) public OnlyOwner {
-		owner[_account] = true;
-	}
-
-
-
-	function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        internal
-        override(ERC1155, ERC1155Supply)
-    {
-       super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+        owner[_account] = true;
     }
 
- //RESOLVE REMIX ISSUES   
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal override(ERC1155, ERC1155Supply) {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    }
+
+    //RESOLVE REMIX ISSUES
     // function _mint(address account, uint256 id, uint256 amount, bytes memory data)
     //     internal
     //     override(ERC1155, ERC1155Supply)
@@ -127,5 +126,4 @@ contract RahatERC1155 is ERC1155,ERC1155Supply,ERC1155Burnable{
     // {
     //     super._burnBatch(account,ids,amounts);
     // }
-
 }
--- contracts/RahatERC20.sol
+++ contracts/RahatERC20.sol
@@ -2,50 +2,51 @@
 pragma solidity 0.8.7;
 
 //ERC20 Tokens
-import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol';
-import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
-
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 //Utils
-import '@openzeppelin/contracts/access/AccessControl.sol';
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract RahatERC20 is ERC20,ERC20Snapshot,ERC20Burnable{    
-    
-	///@dev owner of the ERC20 contract 
-	mapping(address => bool) public owner;
+contract RahatERC20 is ERC20, ERC20Snapshot, ERC20Burnable {
+    ///@dev owner of the ERC20 contract
+    mapping(address => bool) public owner;
 
-	modifier OnlyOwner {
-		require(owner[tx.origin], 'Only Admin can execute this transaction');
-		_;
-	}
+    modifier OnlyOwner() {
+        require(owner[msg.sender], "Only Admin can execute this transaction");
+        _;
+    }
 
-	constructor(
-		string memory _name,
-		string memory _symbol,
-		address _admin
-	)  ERC20(_name, _symbol) {
-		owner[msg.sender] = true;
-		owner[_admin] = true;
-		_mint(msg.sender, 10000);
-	}
-	
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _admin
+    ) ERC20(_name, _symbol) {
+        owner[msg.sender] = true;
+        owner[_admin] = true;
+        _mint(msg.sender, 10000);
+    }
 
-	///@dev Mint x amount of ERC20 token to given address
-	///@param _address Address to which ERC20 token will be minted
-	///@param _amount Amount of token to be minted
-	function mintERC20(address _address, uint256 _amount) public OnlyOwner returns (uint256) {
-		_mint(_address, _amount);
-		return _amount;
-	}
+    ///@dev Mint x amount of ERC20 token to given address
+    ///@param _address Address to which ERC20 token will be minted
+    ///@param _amount Amount of token to be minted
+    function mintERC20(
+        address _address,
+        uint256 _amount
+    ) public OnlyOwner returns (uint256) {
+        _mint(_address, _amount);
+        return _amount;
+    }
 
-	function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        override(ERC20, ERC20Snapshot)
-    {
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Snapshot) {
         super._beforeTokenTransfer(from, to, amount);
     }
-    
-	function addOwner(address _account) public OnlyOwner {
-		owner[_account] = true;
-	}
+
+    function addOwner(address _account) public OnlyOwner {
+        owner[_account] = true;
+    }
 }
